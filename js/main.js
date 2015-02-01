f9111f30f0ad2d6b2dc9d773983bc286e8d31573
Jackson de Campos <jacksondecampos@gmail.com>
2015-02-01 13:12:09 -0800

    Add possibility for multiple mistakes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var SUCCESS_COLOR = '#00c76a';
 var FAIL_COLOR = '#e60b00';
 var ADD_PROBABILITY = 0.5;
-var ADDED_HEIGHT = 32;
+var KEEP_GOING_PROBABILITY = 0.625;
+var ADDED_HEIGHT = 32; //2rem for margin
 var ALL_ICON_CLASSES = 'hidden fa-check fa-times fa-exclamation-triangle';
 
 $(document).ready(function() {
@@ -59,64 +60,64 @@ $(document).ready(function() {
       }
     });
 
-    var methodSwitch = Math.random();
-
     if(allSolved) { //make something wrong
-      var possibilities = [];
-      $('.problem').each(function() {
-        if(!visible(this)) {
-          possibilities.push(this);
-        }
-      });
+      var keepGoingSwitch = 0; //go at least once
 
-      if(possibilities.length === 0) {
-        alert('You win!');
-      } else {
-        var problem = $(possibilities[Math.floor(Math.random()*possibilities.length)]);
+      while(keepGoingSwitch < KEEP_GOING_PROBABILITY) {
+        keepGoingSwitch = Math.random(); //set to random for next time
 
-        if(methodSwitch < ADD_PROBABILITY) { //add a problem
-
-          var beforeIndex = parseFloat($(problem).find('.num').text());
-          var afterIndex = parseFloat($(problem).next().find('.num').text());
-          var newIndex;
-
-          if(beforeIndex && afterIndex) {
-            //average
-            newIndex = beforeIndex + ( (afterIndex - beforeIndex) / 2 );
-          } else if(!beforeIndex) {
-            newIndex = afterIndex - 1;
-          } else if(!afterIndex) {
-            newIndex = beforeIndex + 1;
+        var possibilities = [];
+        $('.problem').each(function() {
+          if(!visible(this)) {
+            possibilities.push(this);
           }
+        });
 
-          problem.after(getProblemHTML(generateProblem(), newIndex));
+        if(possibilities.length === 0) {
+          mmm('You win!');
+        } else {
+          var problem = $(possibilities[Math.floor(Math.random()*possibilities.length)]);
 
-          if(problem.offset().top < $(window).scrollTop()) {
-            var originalHeight = parseInt($('.main').scrollTop());
-            var problemHeight = parseInt($('.problem').outerHeight());
-            console.log('above!');
-            console.log('original is ' + originalHeight);
-            console.log('addition is ' + parseInt(problemHeight + 16));
-            $('.main').scrollTop( originalHeight + problemHeight + ADDED_HEIGHT); //add 1rem, who knows why
-            console.log('new is ' + $('.main').scrollTop());
-          } else {
-            console.log('below!');
+          var methodSwitch = Math.random();
+
+          if(methodSwitch < ADD_PROBABILITY) { //add a problem
+
+            var beforeIndex = parseFloat($(problem).find('.num').text());
+            var afterIndex = parseFloat($(problem).next().find('.num').text());
+            var newIndex;
+
+            if(beforeIndex && afterIndex) {
+              //average
+              newIndex = beforeIndex + ( (afterIndex - beforeIndex) / 2 );
+            } else if(!beforeIndex) {
+              newIndex = afterIndex - 1;
+            } else if(!afterIndex) {
+              newIndex = beforeIndex + 1;
+            }
+
+            problem.after(getProblemHTML(generateProblem(), newIndex));
+
+            if(problem.offset().top < $(window).scrollTop()) {
+              var originalHeight = parseInt($('.main').scrollTop());
+              var problemHeight = parseInt($('.problem').outerHeight());
+              $('.main').scrollTop( originalHeight + problemHeight + ADDED_HEIGHT);
+            }
+
+            //updateIndices();
+          } else { //change a problem
+
+            //generate until we get a different one
+            var newProblem = null;
+            while(newProblem === null || newProblem.answer === problem.attr('data-answer')) {
+              newProblem = generateProblem();
+            }
+
+            problem.attr('data-solved', false);
+            problem.attr('data-answer', newProblem.answer);
+            problem.find('.problem-text').text(newProblem.problem);
+            problem.find('.fa').css('color', FAIL_COLOR);
+            problem.find('.fa').removeClass('hidden fa-check').addClass('fa-times');
           }
-
-          //updateIndices();
-        } else { //change a problem
-
-          //generate until we get a different one
-          var newProblem = null;
-          while(newProblem === null || newProblem.answer === problem.attr('data-answer')) {
-            newProblem = generateProblem();
-          }
-
-          problem.attr('data-solved', false);
-          problem.attr('data-answer', newProblem.answer);
-          problem.find('.problem-text').text(newProblem.problem);
-          problem.find('.fa').css('color', FAIL_COLOR);
-          problem.find('.fa').removeClass('hidden fa-check').addClass('fa-times');
         }
       }
     }
