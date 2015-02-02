4bbc51d3192c451424c2e546779df6ae3aae8cdc
Jackson de Campos <jacksondecampos@gmail.com>
2015-02-02 14:35:30 -0800

    Switch to logarithms problems, only change things on submit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var SUCCESS_COLOR = '#00c76a';
 var FAIL_COLOR = '#e60b00';
 var ADD_PROBABILITY = 0.5;
-var KEEP_GOING_PROBABILITY = 0.625;
+var KEEP_GOING_PROBABILITY = 0.3;
 var ADDED_HEIGHT = 32; //2rem for margin
 var ALL_ICON_CLASSES = 'hidden fa-check fa-times fa-exclamation-triangle';
 
 $(document).ready(function() {
-  for(var i = 0; i < 10; i++) {
+  for(var i = 0; i < 15; i++) {
     $('.problems').append(getProblemHTML(generateProblem()));
   }
   updateIndices();
@@ -16,6 +16,18 @@ $(document).ready(function() {
   $('body').on('click', '.submit', function() {
     //show modal
     mmm('You need to submit all the problems before you finish.');
+
+    //check all problems solved
+    var allSolved = true;
+    $('.problem').each(function() {
+        if($(this).attr('data-solved') === "false") {
+        allSolved = false;
+      }
+    });
+
+    if(allSolved) { //make something wrong
+      makeSomethingWrong()
+    }
 
     //highlight unfinished
     $('.problem').each(function() {
@@ -38,9 +50,9 @@ $(document).ready(function() {
     var problem = $(e.target.parentNode.parentNode);
     var problemInput = problem.find('input');
     var icon = problem.find('.fa');
-    var guess = problemInput.val();
+    var guess = convertAnswer(problemInput.val());
     var answer = problem.attr('data-answer');
-    if(answer === guess) {
+    if(answer == guess) {
       icon.removeClass(ALL_ICON_CLASSES).addClass('fa-check');
       //problemInput.css('background-color', SUCCESS_COLOR);
       icon.css('color', SUCCESS_COLOR);
@@ -51,78 +63,80 @@ $(document).ready(function() {
       icon.css('color', FAIL_COLOR);
       problem.attr('data-solved', false);
     }
+  });
+});
 
-    //check all problems solved
-    var allSolved = true;
+function convertAnswer(a) {
+  //to decimal
+  var split = a.split('/');
+  if(split.length === 2) {
+    return parseInt(split[0]) / parseInt(split[1]);
+  } else {
+    return a;
+  }
+}
+
+function makeSomethingWrong() {
+  var keepGoingSwitch = 0; //go at least once
+
+  while(keepGoingSwitch < KEEP_GOING_PROBABILITY) {
+    keepGoingSwitch = Math.random(); //set to random for next time
+
+    var possibilities = [];
     $('.problem').each(function() {
-      if($(this).attr('data-solved') === "false") {
-        allSolved = false;
+      if(!visible(this)) {
+        possibilities.push(this);
       }
     });
 
-    if(allSolved) { //make something wrong
-      var keepGoingSwitch = 0; //go at least once
+    if(possibilities.length === 0) {
+      mmm('You win!');
+    } else {
+      var problem = $(possibilities[Math.floor(Math.random()*possibilities.length)]);
 
-      while(keepGoingSwitch < KEEP_GOING_PROBABILITY) {
-        keepGoingSwitch = Math.random(); //set to random for next time
+      var methodSwitch = Math.random();
 
-        var possibilities = [];
-        $('.problem').each(function() {
-          if(!visible(this)) {
-            possibilities.push(this);
-          }
-        });
+      if(methodSwitch < ADD_PROBABILITY) { //add a problem
 
-        if(possibilities.length === 0) {
-          mmm('You win!');
-        } else {
-          var problem = $(possibilities[Math.floor(Math.random()*possibilities.length)]);
+        var beforeIndex = parseFloat($(problem).find('.num').text());
+        var afterIndex = parseFloat($(problem).next().find('.num').text());
+        var newIndex;
 
-          var methodSwitch = Math.random();
-
-          if(methodSwitch < ADD_PROBABILITY) { //add a problem
-
-            var beforeIndex = parseFloat($(problem).find('.num').text());
-            var afterIndex = parseFloat($(problem).next().find('.num').text());
-            var newIndex;
-
-            if(beforeIndex && afterIndex) {
-              //average
-              newIndex = beforeIndex + ( (afterIndex - beforeIndex) / 2 );
-            } else if(!beforeIndex) {
-              newIndex = afterIndex - 1;
-            } else if(!afterIndex) {
-              newIndex = beforeIndex + 1;
-            }
-
-            problem.after(getProblemHTML(generateProblem(), newIndex));
-
-            if(problem.offset().top < $(window).scrollTop()) {
-              var originalHeight = parseInt($('.main').scrollTop());
-              var problemHeight = parseInt($('.problem').outerHeight());
-              $('.main').scrollTop( originalHeight + problemHeight + ADDED_HEIGHT);
-            }
-
-            //updateIndices();
-          } else { //change a problem
-
-            //generate until we get a different one
-            var newProblem = null;
-            while(newProblem === null || newProblem.answer === problem.attr('data-answer')) {
-              newProblem = generateProblem();
-            }
-
-            problem.attr('data-solved', false);
-            problem.attr('data-answer', newProblem.answer);
-            problem.find('.problem-text').text(newProblem.problem);
-            problem.find('.fa').css('color', FAIL_COLOR);
-            problem.find('.fa').removeClass('hidden fa-check').addClass('fa-times');
-          }
+        if(beforeIndex && afterIndex) {
+          //average
+          newIndex = beforeIndex + ( (afterIndex - beforeIndex) / 2 );
+        } else if(!beforeIndex) {
+          newIndex = afterIndex - 1;
+        } else if(!afterIndex) {
+          newIndex = beforeIndex + 1;
         }
+
+        problem.after(getProblemHTML(generateProblem(), newIndex));
+
+        if(problem.offset().top < $(window).scrollTop()) {
+          var originalHeight = parseInt($('.main').scrollTop());
+          var problemHeight = parseInt($('.problem').outerHeight());
+          $('.main').scrollTop( originalHeight + problemHeight + ADDED_HEIGHT);
+        }
+
+        //updateIndices();
+      } else { //change a problem
+
+        //generate until we get a different one
+        var newProblem = null;
+        while(newProblem === null || newProblem.answer === problem.attr('data-answer')) {
+          newProblem = generateProblem();
+        }
+
+        problem.attr('data-solved', false);
+        problem.attr('data-answer', newProblem.answer);
+        problem.find('.problem-text').text(newProblem.problem);
+        problem.find('.fa').css('color', FAIL_COLOR);
+        problem.find('.fa').removeClass('hidden fa-check').addClass('fa-times');
       }
     }
-  });
-});
+  }
+}
 
 function visible(element) {
   var height = document.documentElement.clientHeight,
@@ -136,39 +150,22 @@ function visible(element) {
 }
 
 function generateProblem() {
-  var operators = ['+', '-', '*', '/'];
-  var operator = operators[Math.floor(Math.random()*operators.length)];
 
-  var min, max, num1, num2;
+  var base = Math.floor(Math.random() * 6) + 2;
+  var exponent = Math.floor(Math.random() * 3) + 1;
+  var total = Math.pow(base,exponent);
 
-  if(operator === '+') {
-    min = 0;
-    max = 20;
-    num1 = Math.floor(Math.random() * (max - min + 1)) + min;
-    num2 = Math.floor(Math.random() * (max - min + 1)) + min;
-  } else if(operator === '-') {
-    min = 2; max = 20;
-    num1 = Math.floor(Math.random() * (max - min + 1)) + min;
-    max = num1;
-    num2 = Math.floor(Math.random() * (max - min + 1)) + min;
-  } else if(operator === '*') {
-    min = 0;
-    max = 12;
-    num1 = Math.floor(Math.random() * (max - min + 1)) + min;
-    num2 = Math.floor(Math.random() * (max - min + 1)) + min;
-  } else if(operator === '/') {
-    min1 = 1, min2 = 0; //avoid divide by zero
-    max = 12;
-    factor1 = Math.floor(Math.random() * (max - min1 + 1)) + min1;
-    factor2 = Math.floor(Math.random() * (max - min2 + 1)) + min2;
-    num1 = factor1 * factor2;
-    num2 = factor1;
+  if(Math.random() < 0.5) {
+    base = Math.pow(base, 2);
+    exponent = exponent + " / 2";
   }
 
-  var problem = num1 + ' ' + operator + ' ' + num2;
-  var answer = eval(problem);
+  if(Math.random() < 0.25) {
+    return {problem: base + ' ^ ' + exponent, answer: total};
+  } else {
+    return {problem: 'log ' + base + ' of ' + total, answer: convertAnswer(exponent + "") };
+  }
 
-  return  {answer: answer, problem: problem};
 }
 
 function getProblemHTML(problem, index) {
@@ -183,4 +180,8 @@ function updateIndices() {
   $('.problem').each(function(index) {
     $(this).find('.num').text(index+1);
   });
+}
+
+function log(x, y) {
+  return Math.log(y) / Math.log(x);
 }
